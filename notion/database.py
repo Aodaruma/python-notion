2f46f6d819d7c954b84aa509054f37bc0a8857c6
Aodaruma <skyblue.1007.sakkyoku@gmail.com>
2022-10-30 13:42:50 +0900

    Update database.py

--- notion/database.py
+++ notion/database.py
@@ -9,19 +9,17 @@ from requests import Response
 class DatabaseAPI:
     _client: APIClientBase = attr.ib(repr=False)
 
-    def list_databases(self) -> Response:
+    def list(self) -> Response:
         end_point = '/databases'
 
         return self._client.get(endpoint=end_point)
 
-    def retrieve_databases(self,
-                           database_id: str
-                           ) -> Response:
+    def retrieve(self, database_id: str) -> Response:
         end_point = f'/databases/{database_id}'
 
         return self._client.get(endpoint=end_point)
 
-    def filter_database(self,
+    def filter(self,
                         database_id: str,
                         body: dict
                         ) -> Response:
@@ -29,9 +27,9 @@ class DatabaseAPI:
 
         return self._client.post(endpoint=end_point, body=body)
 
-    def create_database(self,
+    def create(self,
                         body: dict
                         ) -> Response:
         end_point = '/databases'
 
-        return self._client.post(endpoint=end_point, body=body)
+        return self._client.post(endpoint=end_point, body=body))
